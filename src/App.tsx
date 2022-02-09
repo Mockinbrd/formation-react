83d039d7ea1c3e3cbf6a8cf39800d7d294a7d9ee
Mockinbrd <marvyn.ap@gmail.com>
2022-02-09 09:18:30 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CurrentSeason from "./components/currentSeason";
+
+function App() {
+    return (
+        <div className="container">
+            <CurrentSeason />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/currentSeason/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CurrentSeason(props) {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default CurrentSeason;
